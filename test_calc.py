eb21459bff8579314597aaff5ede4872b5f75b04
donalte <donaldtedom0@gmail.com>
2023-10-07 15:55:06 +0100

    feat: test calc with assertEqual

---
+++ test_calc.py
@@ -0,0 +1,47 @@
+import unittest 
+import app 
+
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self) -> None:
+        result = app.add(10, 5)
+        self.assertEqual(result, 15)
+ 
+    
+    def test_sub(self) -> None:
+        result = app.substract(10, 5)
+        self.assertEqual(result, 5)
+
+    
+    def test_mul(self) -> None:
+        result = app.multiply(10, 5)
+        self.assertEqual(result, 50)
+
+    
+    def test_div(self) -> None:
+        result1 = app.divide(10, 5)
+        result2 = app.divide(5, 2)
+        self.assertEqual(result1, 2)
+        self.assertEqual(result2, 2.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
