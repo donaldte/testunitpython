f31502340129d5eb08074638641e166be6cc1def
donalte <donaldtedom0@gmail.com>
2023-10-07 17:55:44 +0100

    feat: how to use mock test

--- employee.py
+++ employee.py
@@ -1,3 +1,4 @@
+import requests 
 class Employee:
     """ A sample Employee class """
 
@@ -23,5 +24,12 @@ class Employee:
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
+
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        return response.status_code      
 
     
--- test_employee.py
+++ test_employee.py
@@ -1,5 +1,5 @@
 import unittest
-
+from unittest.mock import patch
 
 from employee import Employee 
 
@@ -9,7 +9,7 @@ class TestEmployee(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         print('setupClass')
-        
+
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -64,6 +64,28 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_1.pay, 52500)
         self.assertEqual(self.emp_2.pay, 63000)
 
+
+    def test_monthly_schedule(self):
+
+        with patch('employee.requests.get') as mocked_get:
+
+            mocked_get.return_value.ok = True 
+            mocked_get.return_value.text = 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/tedom/May')
+
+            self.assertEqual(schedule, 'Success')
+
+
+            mocked_get.return_value.ok = False 
+            mocked_get.return_value.status_code = 400
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/smith/June')
+
+            self.assertEqual(schedule, 400)
+        
 
 
 if __name__ == '__main__':
